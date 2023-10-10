4565fd1adc046c71ea3a34409392753b2e312a92
RussianMartabak <meansproducer02@gmail.com>
2023-10-10 22:21:07 +0700

    added debug and release APK

--- core/src/main/java/com/martabak/core/util/GlobalUtils.kt
+++ core/src/main/java/com/martabak/core/util/GlobalUtils.kt
@@ -112,6 +112,6 @@ object GlobalUtils {
     val ACCESS_TOKEN = "access_token"
     val USERNAME = "username"
     val REFRESH_TOKEN = "refresh_token"
-    val BASE_URL = "http://192.168.1.100:8000"
+    val BASE_URL = "http://192.168.153.125:5000"
     val NIGHT_MODE = "night_mode"
 }
